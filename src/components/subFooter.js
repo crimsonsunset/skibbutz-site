647d1b3f9f856cf5b967c47f3a0c86948be4ffea
Joe Sangiorgio <jsangio1@gmail.com>
2022-01-21 16:00:30 -0500

    add tour page

--- src/components/subFooter.js
+++ src/components/subFooter.js
@@ -2,11 +2,12 @@ import React from 'react'
 import {Container} from 'reactstrap'
 
 let SubFooter = ({title}) => (
+
   <div className="bg-light">
     <Container
       style={{ margin: "0 auto", textAlign:"right", marginRight: "0px"}}
       className="pb-3">
-      <span className="text-secondary small">Copyright © {new Date().getFullYear()}. {title}. All rights reserved.</span>
+      <span className="text-secondary small">Copyright Skibbutz © {new Date().getFullYear()}. All rights reserved.</span>
     </Container>
   </div>
 )
