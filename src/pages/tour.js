af54a7864b81505e46bed59b0a5ffef36a54b7e4
Joseph Sangiorgio <jsangi@icims.com>
2022-02-04 11:12:32 -0700

    test rm bootstrap in stupid place

--- src/pages/tour.js
+++ src/pages/tour.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "@components/header"
 import Footer from "@components/footer"
 import SubFooter from "@components/subFooter"
